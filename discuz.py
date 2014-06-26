c733df1d433b10e57af6d2c4befecf4c89e22bf3
Dapeng Wei <wdp137@gmail.com>
2014-06-26 17:34:41 +0800

    add filter

--- discuz.py
+++ discuz.py
@@ -80,14 +80,49 @@ if __name__ == '__main__':
 		if i in delete_id or i in feedback_tag_stat_detail["火狐阅读"]:
 			feedback_item["fid"] = 0
 			feedback_item["typeid"] = 0
-		else:
+		elif i in feedback_tag_stat_detail["NULLX"]:
+			pass
+		elif i in feedback_tag_stat_detail["校园活动"]:
+			feedback_item["fid"] = 51
+			feedback_item["typeid"] = 0
+		elif i in feedback_tag_stat_detail["活动"]:
+			feedback_item["fid"] = 50
+			feedback_item["typeid"] = 0
+		elif i in feedback_tag_stat_detail["核心用户组"]:
+			feedback_item["fid"] = 52
+			feedback_item["typeid"] = 0
+		elif i in feedback_tag_stat_detail["火狐水区"] or i in feedback_tag_stat_detail["火狐商店"]:
+			feedback_item["fid"] = 53
+			feedback_item["typeid"] = 0
+		elif i in feedback_tag_stat_detail["社区帮助"] or i in feedback_tag_stat_detail["火狐社区帮助"]:
+			feedback_item["fid"] = 55
+			if i in ["30435","30432","30434","30421"]:
+				feedback_item["typeid"] = 35
+			else:
+				feedback_item["typeid"] = 32
+		elif i in feedback_tag_stat_detail["壁纸"] or i in feedback_tag_stat_detail["火狐壁纸"]:
+			feedback_item["fid"] = 54
+			feedback_item["typeid"] = 0
+		elif i in feedback_tag_stat_detail["Thunderbird"] or i in feedback_tag_stat_detail["雷鸟"]:
+			feedback_item["fid"] = 43
+			feedback_item["typeid"] = 20
+		elif i in feedback_tag_stat_detail["Thunderbird"] or i in feedback_tag_stat_detail["雷鸟"]:
+			feedback_item["fid"] = 43
+			feedback_item["typeid"] = 20
+		
+		
+		
+		else: 
 			feedback_item["fid"] = 40
 			feedback_item["typeid"] = 6
+
 		feedback_list.append(feedback_item.values())
+		
 	print "step4"
+
 	with file("discuz_output.txt","a") as feedback_output:
 		write_string = ""
 		for i in feedback_list:
 			write_string = write_string + str(i[1]) +","+str(i[2])+","+str(i[0])+"\n"
-		feedback_output.write(write_string)	
+		feedback_output.write(write_string)
 		
