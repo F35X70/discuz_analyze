1b65cf57d74ca7439beb37fb4c6bf9fc9dc70412
Dapeng Wei <wdp137@gmail.com>
2014-06-27 18:16:52 +0800

    this branch for test filter

--- discuz.py
+++ discuz.py
@@ -2,7 +2,7 @@
 import readfile_discuz
 import jieba
 if __name__ == '__main__':
-	print "step1"
+
 	feedback_name = "discuz.txt"
 	file_content = readfile_discuz.ReadContent(feedback_name)
 	feedback_comment_stat = {}
@@ -17,12 +17,12 @@ if __name__ == '__main__':
 		del_read_content = del_read.read()
 		del_read_content = del_read_content.strip("\n")
 		delete_id = del_read_content.split(";")
-	print "step2"
+
 	for i in file_content:
 		
 		#feedback_id
 		feedback_id.append(i[0])
-		
+		"""
 		#discuz_stat
 		seg_list = jieba.cut_for_search(i[1])
 		for x in seg_list:
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 				except KeyError:
 					feedback_comment_stat_detail[x] = []
 					feedback_comment_stat_detail[x].append(i[0])
-		
+		"""
 		#tag_stat
 		for feedback_tag in i[-1]:
 			try:
@@ -73,51 +73,14 @@ if __name__ == '__main__':
 			#	write_string = write_string + str(x) + ","
 		tag_stat.write(write_string)
 	"""
-	print "step3"
+
 	for i in feedback_id:
 		feedback_item = {"id":0,"fid":0,"typeid":0}
 		feedback_item["id"] = i
-		if i in delete_id or i in feedback_tag_stat_detail["火狐阅读"]:
-			feedback_item["fid"] = 0
-			feedback_item["typeid"] = 0
-		elif i in feedback_tag_stat_detail["NULLX"]:
-			pass
-		elif i in feedback_tag_stat_detail["校园活动"]:
-			feedback_item["fid"] = 51
-			feedback_item["typeid"] = 0
-		elif i in feedback_tag_stat_detail["活动"]:
-			feedback_item["fid"] = 50
-			feedback_item["typeid"] = 0
-		elif i in feedback_tag_stat_detail["核心用户组"]:
-			feedback_item["fid"] = 52
-			feedback_item["typeid"] = 0
-		elif i in feedback_tag_stat_detail["火狐水区"] or i in feedback_tag_stat_detail["火狐商店"]:
-			feedback_item["fid"] = 53
-			feedback_item["typeid"] = 0
-		elif i in feedback_tag_stat_detail["社区帮助"] or i in feedback_tag_stat_detail["火狐社区帮助"]:
-			feedback_item["fid"] = 55
-			if i in ["30435","30432","30434","30421"]:
-				feedback_item["typeid"] = 35
-			else:
-				feedback_item["typeid"] = 32
-		elif i in feedback_tag_stat_detail["壁纸"] or i in feedback_tag_stat_detail["火狐壁纸"]:
-			feedback_item["fid"] = 54
-			feedback_item["typeid"] = 0
-		elif i in feedback_tag_stat_detail["Thunderbird"] or i in feedback_tag_stat_detail["雷鸟"]:
-			feedback_item["fid"] = 43
-			feedback_item["typeid"] = 20
-		elif i in feedback_tag_stat_detail["Thunderbird"] or i in feedback_tag_stat_detail["雷鸟"]:
-			feedback_item["fid"] = 43
-			feedback_item["typeid"] = 20
+		if i in feedback_tag_stat_detail["开发者问答"] :
+			print i 
 		
-		
-		
-		else: 
-			feedback_item["fid"] = 40
-			feedback_item["typeid"] = 6
-
-		feedback_list.append(feedback_item.values())
-		
+	"""	
 	print "step4"
 
 	with file("discuz_output.txt","a") as feedback_output:
@@ -125,4 +88,4 @@ if __name__ == '__main__':
 		for i in feedback_list:
 			write_string = write_string + str(i[1]) +","+str(i[2])+","+str(i[0])+"\n"
 		feedback_output.write(write_string)
-		
+	"""
