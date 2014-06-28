9f4718957e60db360be417fffe77c7634d5b77de
Dapeng Wei <wdp137@gmail.com>
2014-06-28 18:01:35 +0800

    add filter

--- discuz.py
+++ discuz.py
@@ -5,8 +5,6 @@ if __name__ == '__main__':
 
 	feedback_name = "discuz.txt"
 	file_content = readfile_discuz.ReadContent(feedback_name)
-	feedback_comment_stat = {}
-	feedback_comment_stat_detail = {}
 	feedback_tag_stat = {}
 	feedback_tag_stat_detail = {}
 	feedback_id = []
@@ -22,22 +20,7 @@ if __name__ == '__main__':
 		
 		#feedback_id
 		feedback_id.append(i[0])
-		"""
-		#discuz_stat
-		seg_list = jieba.cut_for_search(i[1])
-		for x in seg_list:
-			x = x.encode("utf-8")
-			if  len(x) != 1 and len(x) != 3 :
-				try:
-					feedback_comment_stat[x] = feedback_comment_stat[x] + 1
-				except KeyError:
-					feedback_comment_stat[x] = 1
-				try:
-					feedback_comment_stat_detail[x].append(i[0])
-				except KeyError:
-					feedback_comment_stat_detail[x] = []
-					feedback_comment_stat_detail[x].append(i[0])
-		"""
+		
 		#tag_stat
 		for feedback_tag in i[-1]:
 			try:
@@ -50,48 +33,78 @@ if __name__ == '__main__':
 				feedback_tag_stat_detail[feedback_tag] = []
 				feedback_tag_stat_detail[feedback_tag].append(i[0])
 
-	for i in feedback_comment_stat_detail:
-		feedback_comment_stat_detail[i] = list(set(feedback_comment_stat_detail[i]))
-	"""
-	#output
-	feedback_comment_stat_sort =  sorted(feedback_comment_stat.iteritems(), key=lambda d:d[1], reverse = True)
-	feedback_tag_stat_sort =  sorted(feedback_tag_stat.iteritems(), key=lambda d:d[1], reverse = True)
-	
-	with file("discuz_comment_stat.txt","a") as feedback_stat:
-		write_string = ""
-		for i in feedback_comment_stat_sort:
-				write_string = write_string + i[0]+";"+str(i[1])+";"+"\n"
-				#for x in feedback_comment_stat_detail[i[0]]:
-				#	write_string = write_string + str(x) + ","
-		feedback_stat.write(write_string)
-	
-	with file("discuz_tag_stat.txt","a") as tag_stat:
-		write_string = ""
-		for i in feedback_tag_stat_sort:
-			write_string = write_string + i[0]+";"+str(i[1])+";"+"\n"
-			#for x in feedback_tag_stat_detail[i[0]]:
-			#	write_string = write_string + str(x) + ","
-		tag_stat.write(write_string)
-	"""
-
 	for i in feedback_id:
 		feedback_item = {"id":0,"fid":0,"typeid":0}
 		feedback_item["id"] = i
-		if i in feedback_tag_stat_detail["Firefox OS"]:
-			for x in file_content: 
-				if i == x[0]:
-					#print x[1].decode("utf-8")
-					for z in x[-1]:
-						if z != "Firefox OS":
-							print z.decode("utf-8"),
-					print ""
-			
-	"""	
-	print "step4"
-
-	with file("discuz_output.txt","a") as feedback_output:
-		write_string = ""
+		if i in feedback_tag_stat_detail["火狐阅读"] or i in delete_id:
+			pass
+		elif i in feedback_tag_stat_detail["壁纸"]:
+			feedback_item["fid"] = 54
+		elif i in feedback_tag_stat_detail["火狐壁纸"]:
+			feedback_item["fid"] = 54
+		elif i in feedback_tag_stat_detail["校园活动"]:
+			feedback_item["fid"] = 51
+		elif i in feedback_tag_stat_detail["活动"]:
+			feedback_item["fid"] = 50
+		elif i in feedback_tag_stat_detail["核心用户组"]:
+			feedback_item["fid"] = 52
+		elif i in feedback_tag_stat_detail["火狐水区"]:
+			feedback_item["fid"] = 53
+		elif i in feedback_tag_stat_detail["火狐商店"]:
+			feedback_item["fid"] = 53
+		elif i in feedback_tag_stat_detail["社区帮助"]:
+			feedback_item["fid"] = 55
+			feedback_item["typeid"] = 6
+		elif i in feedback_tag_stat_detail["火狐社区帮助"]:
+			feedback_item["fid"] = 55
+			feedback_item["typeid"] = 6
+		elif i in feedback_tag_stat_detail["Thunderbird"]:
+			feedback_item["fid"] = 43
+		elif i in feedback_tag_stat_detail["雷鸟"]:
+			feedback_item["fid"] = 43
+		elif i in feedback_tag_stat_detail["官方博客"]:
+			feedback_item["fid"] = 2
+		elif i in feedback_tag_stat_detail["火狐快讯"]:
+			feedback_item["fid"] = 37
+		elif i in feedback_tag_stat_detail["火狐移动版"]:
+			feedback_item["fid"] = 42
+		elif i in feedback_tag_stat_detail["javascript"]:
+			feedback_item["fid"] = 47
+		elif i in feedback_tag_stat_detail["JS"]:
+			feedback_item["fid"] = 47
+		elif i in feedback_tag_stat_detail["css"]:
+			feedback_item["fid"] = 47
+		elif i in feedback_tag_stat_detail["html5"]:
+			feedback_item["fid"] = 47
+		elif i in feedback_tag_stat_detail["Firefox OS"] and i in feedback_tag_stat_detail["web app"]:
+			feedback_item["fid"] = 46
+		elif i in feedback_tag_stat_detail["Firefox OS"] and i in feedback_tag_stat_detail["开发者学堂"]:
+			feedback_item["fid"] = 46
+		elif i in feedback_tag_stat_detail["Firefox OS"] and i in feedback_tag_stat_detail["开发者问答"]:
+			feedback_item["fid"] = 46
+		elif i in feedback_tag_stat_detail["Firefox OS"]:
+			feedback_item["fid"] = 41
+		elif i in feedback_tag_stat_detail["开发者问答"]:
+			feedback_item["fid"] = 45
+		elif i in feedback_tag_stat_detail["兼容性"]:
+			feedback_item["fid"] = 40
+			feedback_item["typeid"] = 4
+		elif i in feedback_tag_stat_detail["插件"]:
+			feedback_item["fid"] = 40
+			feedback_item["typeid"] = 3
+		elif i in feedback_tag_stat_detail["第三方插件"]:
+			feedback_item["fid"] = 40
+			feedback_item["typeid"] = 3
+		elif i in feedback_tag_stat_detail["扩展"]:
+			feedback_item["fid"] = 40
+			feedback_item["typeid"] = 2
+		else:
+			feedback_item["fid"] = 40
+			feedback_item["typeid"] = 1
+		feedback_list.append(str(feedback_item["id"])+","+str(feedback_item["fid"])+","+str(feedback_item["typeid"]))
+		
+	with file("discuz_import.txt","a") as discuz_import:
+		write_cache = ""
 		for i in feedback_list:
-			write_string = write_string + str(i[1]) +","+str(i[2])+","+str(i[0])+"\n"
-		feedback_output.write(write_string)
-	"""
+			write_cache = write_cache + i +"\n"
+		discuz_import.write(write_cache)
